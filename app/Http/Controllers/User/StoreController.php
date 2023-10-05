b30c358074bb285613b54c1d1c6567e704a8bc00
nikitanikolaev <vasiti87@gmail.com>
2023-10-05 17:51:02 +0300

    Установка JWT Auth. Aсcess(Login) token и Refresh token, token expired

--- app/Http/Controllers/User/StoreController.php
+++ app/Http/Controllers/User/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\StoreRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class StoreController extends Controller
@@ -12,6 +13,7 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
         User::firstOrCreate([
             'email' => $data['email']
         ], $data);
--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [\App\Http\Controllers\AuthController::class, 'me']);
+
+    Route::group(['namespace' => 'App\Http\Controllers\Fruit', 'prefix' => 'fruits'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
+
 Route::group(['namespace' => 'App\Http\Controllers\User', 'prefix' => 'users'],function () {
     Route::post('/', 'StoreController');
 });
-
-Route::group(['namespace' => 'App\Http\Controllers\Fruit', 'prefix' => 'fruits'], function () {
-    Route::get('/', 'IndexController');
-});
-
-
 
