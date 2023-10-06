b4ee28f4973797f523759d73e3d17a62da853a05
nikitanikolaev <vasiti87@gmail.com>
2023-10-06 12:37:16 +0300

    Получаем refresh token используя axios запрос внутри interceptors

--- resources/js/api.js
+++ resources/js/api.js
@@ -5,21 +5,30 @@ const api = axios.create();
 
 api.interceptors.request.use( config => {
     if (localStorage.getItem('access_token')) {
-        config.headers = {
-            'authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
+        config.headers.authorization = `Bearer ${localStorage.getItem('access_token')}`
     }
     return config
 }, error => {})
 
 api.interceptors.response.use( config => {
     if (localStorage.getItem('access_token')) {
-        config.headers = {
-            'authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
+        config.headers.authorization = `Bearer ${localStorage.getItem('access_token')}`
     }
     return config
 }, error => {
+    if (error.data.message === 'Token has expired') {
+        axios.post('/api/auth/refresh/', {}, {
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('access_token')}`,
+            }
+        })
+            .then( res => {
+                localStorage.setItem('access_token', res.data.access_token)
+                error.config.headers.authorization = `Bearer ${res.data.access_token}`
+                return api.request(error.config)
+            })
+    }
+
     if (error.response.status === 401) {
         router.push({ name: 'user.login' })
     }
